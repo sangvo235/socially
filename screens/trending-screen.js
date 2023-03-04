a7ed3a6f21009621f38df9fa73933d5a9e7e9fbb
Sang Vo <sangvo235@gmail.com>
2023-03-05 01:56:26 +1100

    update

--- screens/trending-screen.js
+++ screens/trending-screen.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native';
+import EventList from "../components/events/event-list";
 
 const TrendingScreen = () => {
 
     return (
-        <View>
-            <Text>Trending yay!!</Text>
+        <View style={{backgroundColor: 'white', height: 1600, padding: 25}}>
+            <EventList />
         </View>
 
     );
