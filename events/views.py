da270748e393c29d5bb2c0c5f8f8fc068bd35c21
Sang Vo <sangvo235@gmail.com>
2023-03-05 14:50:31 +1100

    update

---
+++ events/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Topics
+from .serializers import TopicsSerializer
+from rest_framework import viewsets
+from django.views.decorators.csrf import csrf_exempt
+
+
+def index(request):
+    Topics_list = Topics.objects.all()
+    context = {'Topics_list': Topics_list}
+    return render(request, 'socially/index.html', context)
+
+
+# Rest api end point
+def get_rest_list(request):
+    """
+    Returns Json list of all Topics
+    """
+    if request.method == "GET":
+        Topics_list = Topics.objects.order_by('id')
+        serializer = TopicsSerializer(Topics_list, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+# Create your views here.
